a7291eb7e5f3df941a69408f8989a1617d360567
Jyoti Saha <josaha1997@gmail.com>
2020-10-16 15:11:39 +0530

    update

--- questions.js
+++ questions.js
@@ -1,32 +1,65 @@
+var input1 = document.createElement("INPUT");
+input1.id = "answer1";
+input1.style.width = "40px";
+
+var input2 = document.createElement("INPUT");
+input2.id = "answer2";
+input2.style.width = "40px";
+
+var btn = document.createElement("BUTTON");
+btn.innerHTML = "Submit";
+btn.style.marginTop = "10px";
+
+var quest = document.createElement("P");
+quest.style.marginLeft = "20%";
+quest.style.marginRight = "20%";
+quest.style.color = "white";
+quest.style.fontSize = "30px";
+
+var line = document.createElement("HR");
+line.style.width = "50px";
+
+var correct = document.createElement("IMG");
+correct.src = "images/correct.gif";
+correct.style.width = "50%%";
+correct.style.height = "30%";
+
+var wrong = document.createElement("IMG");
+wrong.src = "images/wrong.gif";
+wrong.style.width = "20%";
+wrong.style.height = "20%";
+
+var div = document.createElement("DIV");
+div.classList.add("frac");
+div.id = "frac";
+
+var span = document.createElement("SPAN");
+span.innerHTML = "4";
+
+var span1 = document.createElement("SPAN");
+span1.classList.add("symbol");
+span1.innerHTML = "/";
+
+var span2 = document.createElement("SPAN");
+span2.classList.add("bottom");
+span2.innerHTML = "10";
+
+var span3 = document.createElement("SPAN");
+span3.style.marginLeft = "10px";
+span3.style.marginRight = "10px";
+span3.innerHTML = "X";
+
+var span4 = document.createElement("SPAN");
+span4.innerHTML = "100";
+
+var span5 = document.createElement("SPAN");
+span5.innerHTML = " = ";
+
+var span6 = document.createElement("SPAN");
+span6.innerHTML = " % ";
+
 function questionElements(question) {
-  var input1 = document.createElement("INPUT");
-  input1.id = "answer1";
-  input1.style.width = "40px";
-
-  var quest = document.createElement("P");
-  quest.style.marginLeft = "20%";
-  quest.style.marginRight = "20%";
-  quest.style.color = "white";
-  quest.style.fontSize = "30px";
   quest.innerHTML = question;
-
-  var btn = document.createElement("BUTTON");
-  btn.innerHTML = "Submit";
-  btn.style.marginTop = "10px";
-
-  var line = document.createElement("HR");
-  line.style.width = "40px";
-
-  var correct = document.createElement("IMG");
-  correct.src = "images/correct.gif";
-  correct.style.width = "50%";
-  correct.style.height = "50%";
-
-  var wrong = document.createElement("IMG");
-  wrong.src = "images/wrong.gif";
-  wrong.style.width = "50%";
-  wrong.style.height = "50%";
-
   document.getElementById("questionDiv").style.backgroundImage =
     "url('images/panel1.jpg')";
   document.getElementById("questionDiv").style.backgroundRepeat = "no-repeat";
@@ -36,20 +69,35 @@ function questionElements(question) {
 }
 
 function checkAnswer(checkValue, isEnd) {
+  // alert("hi");
   document.getElementById("questionDiv").append(document.createElement("BR"));
   if (input1.value == checkValue) {
     btn.disabled = true;
     document.getElementById("questionDiv").append(correct);
-
+    input1.value = "";
+    input2.value = "";
     setTimeout(function () {
       isStarted = true;
-      if (!isEnd) start = 1;
+      if (!isEnd) {
+        start = 1;
+        document
+          .getElementById("questionDiv")
+          .append(
+            (document.createElement("H").innerHTML =
+              "You have completed 100% of the game !!!")
+          );
+      }
       score += 2;
-      document.getElementById("questionDiv").innerHTML = "";
+      document
+        .getElementById("questionDiv")
+        .querySelectorAll("*")
+        .forEach((n) => n.remove());
+      //document.getElementById("questionDiv").innerHTML = "";
       document.getElementById("questionDiv").style.backgroundImage = "";
       requestAnimationFrame(draw);
-    }, 1000);
+    }, 3000);
   } else {
+    document.getElementById("questionDiv").append(document.createElement("BR"));
     document.getElementById("questionDiv").append(wrong);
   }
 }
@@ -58,10 +106,6 @@ function quest1() {
   questionElements(
     "If there are total 10 pillar in this game and you have crossed 4 now, then how much part of the game have you completed in fraction?"
   );
-  var input2 = document.createElement("INPUT");
-  input2.id = "answer2";
-  input2.style.width = "30px";
-
   document.getElementById("questionDiv").append(input1);
   document.getElementById("questionDiv").append(document.createElement("BR"));
   document.getElementById("questionDiv").append(line);
@@ -69,33 +113,24 @@ function quest1() {
   document.getElementById("questionDiv").append(input2);
   document.getElementById("questionDiv").append(document.createElement("BR"));
   document.getElementById("questionDiv").append(btn);
-
   btn.onclick = function () {
     if (input2.value == 10) {
       checkAnswer(4, false);
     }
   };
+
+  //btn.removeEventListener("click", ans1);
 }
 function quest2() {
+  document
+    .getElementById("questionDiv")
+    .querySelectorAll("*")
+    .forEach((n) => n.remove());
+  //btn.removeAttribute("onclick");
+
   questionElements(
     "What number should we multiply to your fraction in last question to convert it into percentage?"
   );
-
-  var div = document.createElement("DIV");
-  div.classList.add("frac");
-  div.id = "frac";
-  var span = document.createElement("SPAN");
-  span.innerHTML = "4";
-  var span1 = document.createElement("SPAN");
-  span1.classList.add("symbol");
-  span1.innerHTML = "/";
-  var span2 = document.createElement("SPAN");
-  span2.classList.add("bottom");
-  span2.innerHTML = "10";
-  var span3 = document.createElement("SPAN");
-  span3.style.marginLeft = "10px";
-  span3.style.marginRight = "10px";
-  span3.innerHTML = "X";
 
   document.getElementById("questionDiv").append(div);
   document.getElementById("frac").append(span);
@@ -105,35 +140,15 @@ function quest2() {
   document.getElementById("questionDiv").append(input1);
   document.getElementById("questionDiv").append(document.createElement("BR"));
   document.getElementById("questionDiv").append(btn);
-
-  btn.onclick = checkAnswer(100, false);
+  btn.disabled = false;
+  btn.onclick = function () {
+    checkAnswer(100, false);
+  };
 }
 function quest3() {
   questionElements(
     "What is the percentage of game you completed in first question?"
   );
-
-  var div = document.createElement("DIV");
-  div.classList.add("frac");
-  div.id = "frac";
-  var span = document.createElement("SPAN");
-  span.innerHTML = "4";
-  var span1 = document.createElement("SPAN");
-  span1.classList.add("symbol");
-  span1.innerHTML = "/";
-  var span2 = document.createElement("SPAN");
-  span2.classList.add("bottom");
-  span2.innerHTML = "10";
-  var span3 = document.createElement("SPAN");
-  span3.style.marginLeft = "10px";
-  span3.style.marginRight = "10px";
-  span3.innerHTML = "X";
-  var span4 = document.createElement("SPAN");
-  span4.innerHTML = "100";
-  var span5 = document.createElement("SPAN");
-  span5.innerHTML = " = ";
-  var span6 = document.createElement("SPAN");
-  span6.innerHTML = " % ";
 
   document.getElementById("questionDiv").append(div);
   document.getElementById("frac").append(span);
@@ -146,6 +161,8 @@ function quest3() {
   document.getElementById("questionDiv").append(span6);
   document.getElementById("questionDiv").append(document.createElement("BR"));
   document.getElementById("questionDiv").append(btn);
-
-  btn.onclick = checkAnswer(40, true);
+  btn.disabled = false;
+  btn.onclick = function () {
+    checkAnswer(40, true);
+  };
 }
